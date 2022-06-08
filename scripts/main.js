95c97d97b080699184cc28e02b92bef84c59c1a5
Blockzilla101 <blockzilla101@gmail.com>
2022-06-08 16:49:20 +0500

    .

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
-Events.on(WorldLoadEvent, () => Timer.schedule(() => {
-    if (Vars.state.map.name().toLowerCase().startsWith("bomberman")) {
+Events.on(ClientLoadEvent, () => Timer.schedule(() => {
+    if (Vars.state.map != null && Vars.state.map.name().toLowerCase().startsWith("bomberman")) {
         UnitTypes.nova.canBoost = false
     } else {
         UnitTypes.nova.canBoost = true
     }
-}, 5))
+}, 5, 5))
